d68e1c791b544e9b4a67b8215c536368ddf443e8
JRafaelLobo <115841864+JRafaelLobo@users.noreply.github.com>
2023-01-27 13:35:42 -0600

    Iniciando el Proyecto con menu hecho

---
+++ src/main/java/com/mycompany/lab2p2_joselobo/Lab2P2_JoseLobo.java
@@ -0,0 +1,19 @@
+package com.mycompany.lab2p2_joselobo;
+
+import java.util.Scanner;
+
+public class Lab2P2_JoseLobo {
+    static Scanner leer = new Scanner(System.in);
+    public static void main(String[] args) {
+        int opcion=Menu();
+    }
+    public static int Menu(){
+        System.out.println("""
+                           1. Registro de Inmueble/Solar
+                           2. Manejo de Estados
+                           3. Log In/Sign Up
+                           4. Salir
+                           """);
+        return leer.nextInt();
+    }//menu
+}
